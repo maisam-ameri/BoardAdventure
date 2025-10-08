51aa6b22c819b47f3a2c91cfa03d8b11768065a4
Meysam Ameri <maisamflash@gmail.com>
2025-10-09 01:29:13 +0330

    Update uiMessage

--- Assets/Scripts/UI/UIMessageManager.cs
+++ Assets/Scripts/UI/UIMessageManager.cs
@@ -2,21 +2,27 @@
 
 namespace UI
 {
-    public class UIMessageManager: MonoBehaviour
+    public class UIMessageManager : MonoBehaviour
     {
         [SerializeField] private UIMessage uiMessage;
         [SerializeField] private Transform parent;
 
         public void ShowRollMessage() => ShowMessage("Please roll the dice");
+        
         public void ShowStartNodeMessage() => ShowMessage("The start node isn't empty");
-        public void ShowEnterPawnMessage(string playerName) => ShowMessage($"{playerName} can enter a pawn");
-        public void ShowMovePawnMessage(string playerName) => ShowMessage($"{playerName} can move a pawn");
+        
+        public void ShowPlayerTurnMessage(string playerName) =>
+            ShowMessage($"the turn is {playerName}");
+        
+        public void ShowEndTurnMessage(string playerName)=>
+            ShowMessage($"the turn of {playerName} Ended");
+        
         public void ShowActionAvailableMessage(string playerName, int step) =>
             ShowMessage($"{playerName} rolled {step}! Choose a pawn to move or enter.");
-
+        
         public void ShowRewardMessage(string playerName) =>
             ShowMessage($"{playerName} got 6! Roll again!");
-        
+
         private void ShowMessage(string message, float duration = 1f)
         {
             var uiMsg = GetUiMessage();
@@ -27,7 +33,8 @@ namespace UI
         {
             var uiMsg = Instantiate(uiMessage, parent);
             uiMsg.name = "uiMessage";
-            return  uiMsg;
+            return uiMsg;
         }
+
     }
 }
