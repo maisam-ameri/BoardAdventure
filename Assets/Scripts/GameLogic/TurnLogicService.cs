9eb52f95326643a4ec568c5de02ac23ff6a3ca05
Meysam Ameri <maisamflash@gmail.com>
2025-10-08 14:24:20 +0330

    Create turn logic service

---
+++ Assets/Scripts/GameLogic/TurnLogicService.cs
@@ -0,0 +1,33 @@
+﻿using Players;
+
+namespace GameLogic
+{
+    public enum TurnDecision
+    {
+        WaitForAction,
+        SwitchTurn,
+        RollReward
+    }
+
+    public class TurnLogicService
+    {
+        public bool HasReward { get; set; }
+
+        private TurnDecision ProcessRoll(int? step, Player currentPlayer, bool canEnterPawn, bool canMovePawn)
+        {
+            if (step == 6)
+            {
+                HasReward = true;
+
+                if (canEnterPawn || canMovePawn)
+                    return TurnDecision.WaitForAction;
+
+                return TurnDecision.RollReward;
+            }
+
+            HasReward = false;
+
+            return canMovePawn ? TurnDecision.WaitForAction : TurnDecision.SwitchTurn;
+        }
+    }
+}
