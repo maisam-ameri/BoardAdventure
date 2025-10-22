6de774540c872ab535e71cf6627ad732a9b45860
Meysam Ameri <maisamflash@gmail.com>
2025-10-22 12:37:39 +0330

    Extract player inputs

---
+++ Assets/Scripts/Managers/GameInputHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace BoardAdventures.Managers
+{
+    public class GameInputHandler: MonoBehaviour
+    {
+        public event Action OnDiceRollRequested;
+
+        public void HandleDiceRollRequest()
+        {
+            OnDiceRollRequested?.Invoke();
+        }
+    }
+}
