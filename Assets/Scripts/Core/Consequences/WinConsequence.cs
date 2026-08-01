faf4e836ad42e98f1138427c0720a2fc9cac6bc5
Meysam Ameri <maisamflash@gmail.com>
2026-08-01 18:57:11 +0330

    feat: add win consequence

---
+++ Assets/Scripts/Core/Consequences/WinConsequence.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BoardAdventures.Core.Results;
+using BoardAdventures.Core.State;
+
+namespace BoardAdventures.Core.Consequences
+{
+    public class WinConsequence: IMoveConsequence
+    {
+        public int Order { get; } = ConsequenceOrder.Win;
+        public void Execute(MatchState matchState, MovePawnResult movePawnResult)
+        {
+            var playerId = matchState.BoardState.PawnsState
+                .Find(p =>p.PawnId == movePawnResult.PawnId)
+                .OwnerPlayerId;
+            
+            var arePawnsReachedGoalNode = matchState.BoardState.PawnsState
+                .Where(p => p.OwnerPlayerId == playerId)
+                .Select(p => p.PawnLocationState)
+                .All(p => p == PawnLocationState.InFinalGoal);
+
+            if (arePawnsReachedGoalNode)
+                matchState.WinnerId = playerId;
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/Core/Consequences/WinConsequenceTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using BoardAdventures.Core.Board;
+using BoardAdventures.Core.Consequences;
+using BoardAdventures.Core.Results;
+using BoardAdventures.Core.State;
+using Core.Board;
+using NUnit.Framework;
+
+namespace Tests.EditMode.Core.Consequences
+{
+    public class WinConsequenceTest
+    {
+        
+        private MatchState _matchState;
+        private IBoardDefinition _boardDefinition;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _matchState = TestHelper.CreateMatchState();
+            _boardDefinition = new FakeBoardDefinition {IsFinalGoal = true};
+        }
+
+        [Test]
+        public void Execute_Should_SetWinner_When_AllPlayerPawnsReachedFinalGoal()
+        {
+            // Arrange
+            byte pawnId = 1;
+            var nodeId = 1;
+            var ownerPlayerId = "player_1";
+            var factionType = FactionType.Blue;
+            var locationState = PawnLocationState.InFinalGoal;
+
+            _matchState.BoardState.PawnsState = new List<PawnState>
+            {
+                new PawnState
+                {
+                    NodeId = nodeId, PawnId = pawnId, OwnerPlayerId = ownerPlayerId, FactionType = factionType,
+                    PawnLocationState = locationState
+                },
+                new PawnState
+                {
+                    NodeId = nodeId, PawnId = pawnId, OwnerPlayerId = ownerPlayerId, FactionType = factionType,
+                    PawnLocationState = locationState
+                },
+            };
+
+            var winConsequence = new WinConsequence();
+            var movePawnResult = new MovePawnResult(pawnId, MoveFailReason.None, new[] {1});
+            // Act
+            winConsequence.Execute(_matchState, movePawnResult);
+            
+            // Assert
+            Assert.AreEqual(ownerPlayerId,_matchState.WinnerId);
+        }   
+        
+        
+        [Test]
+        public void Execute_Should_NotSetWinner_When_NotAllPlayerPawnsReachedFinalGoal()
+        {
+            // Arrange
+            byte pawnId = 1;
+            var nodeId = 1;
+            var ownerPlayerId = "player_1";
+            var factionType = FactionType.Blue;
+            var locationState = PawnLocationState.InFinalGoal;
+
+            _matchState.BoardState.PawnsState = new List<PawnState>
+            {
+                new PawnState
+                {
+                    NodeId = nodeId, PawnId = pawnId, OwnerPlayerId = ownerPlayerId, FactionType = factionType,
+                    PawnLocationState = locationState
+                },
+                new PawnState
+                {
+                    NodeId = nodeId, PawnId = pawnId, OwnerPlayerId = ownerPlayerId, FactionType = factionType,
+                    PawnLocationState = PawnLocationState.OnBoard
+                },
+            };
+
+            var winConsequence = new WinConsequence();
+            var movePawnResult = new MovePawnResult(pawnId, MoveFailReason.None, new[] {1});
+            // Act
+            winConsequence.Execute(_matchState, movePawnResult);
+            
+            // Assert
+            Assert.IsNull(_matchState.WinnerId);
+        }  
+        
+    }
+}
