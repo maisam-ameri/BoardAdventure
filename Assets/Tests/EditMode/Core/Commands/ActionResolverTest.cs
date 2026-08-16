2cab67c78203b03b6476229dcd1dca034b1b0ff1
Meysam Ameri <maisamflash@gmail.com>
2026-08-16 15:42:27 +0330

    feat: implement ActionResolver with tests

---
+++ Assets/Tests/EditMode/Core/Commands/ActionResolverTest.cs
@@ -0,0 +1,38 @@
+﻿using BoardAdventures.Core.Commands;
+using BoardAdventures.Core.State;
+using Core.Board;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.EditMode.Core.Commands
+{
+    public class ActionResolverTest
+    {
+        private MatchState _matchState;
+        private ActionResolver _resolver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _matchState = TestHelper.CreateMatchState();
+            _resolver = new ActionResolver();
+        }
+
+        [Test]
+        public void Resolve_Should_ReturnMoveCommand_WhenPlayerSelectOnBoardPawn()
+        {
+            // Arrange
+            byte pawnId = 1;
+            _matchState.BoardState.PawnsState.Add(
+                new PawnState {PawnId = pawnId, PawnLocationState = PawnLocationState.OnBoard}
+            );
+
+            // Act
+            var command = _resolver.Resolve(_matchState, pawnId);
+
+            // Assert
+            Assert.IsInstanceOf<MovePawnCommand>(command);
+            Assert.AreEqual(pawnId,((MovePawnCommand)command).PawnId);
+        }
+    }
+}
